83cf81c96289ae1743d3d03a0c7f027dc74abc39
Miralem.Ba <miralembanovi@gmail.com>
2024-03-10 13:45:34 +0100

    math.js , package.json and math.test.js modified

--- src/math.test.js
+++ src/math.test.js
@@ -1,3 +1,19 @@
-test('adds 1 + 2 to equal 3', () => {
+// math.test.js
+
+const { add, subtract, multiply, divide } = require('./math');
+
+test('addition', () => {
   expect(add(1, 2)).toBe(3);
+});
+
+test('subtraction', () => {
+  expect(subtract(3, 1)).toBe(2);
+});
+
+test('multiplication', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+
+test('division', () => {
+  expect(divide(6, 2)).toBe(3);
 });
